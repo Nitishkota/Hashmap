f6cb6c889bbe0fb9cd0d213340c87592be504bee
Nitishkota <55479057+Nitishkota@users.noreply.github.com>
2024-01-15 11:20:25 -0700

    Add files via upload

---
+++ Newmap.java
@@ -0,0 +1,30 @@
+package mapping;
+
+import java.util.*;
+
+public class Newmap {
+
+	public static void main(String[] args) {
+		
+		HashMap<String, Integer> data = new HashMap<>();
+		
+		data.put("Sek", 200);
+		data.put("Mahi", 40);
+		data.put("Har", 500);
+		
+		
+		HashMap<String, Integer> data1 = new HashMap<String, Integer>(data);
+		
+		
+		data1.put("Nit", 400);
+		
+		data1.remove("Mahitha");
+		
+		
+		System.out.println(data1);
+		
+		
+
+	}
+
+}
